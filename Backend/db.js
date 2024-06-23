344345792663bb19428f8efa4768eaeeb29d38c8
qnasir <qnasir575@gmail.com>
2024-06-23 12:34:39 +0530

    removed

--- Backend/db.js
+++ Backend/db.js
@@ -1,14 +1,1 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
 
-const connectDb = async () => {
-  try {
-    await mongoose.connect(process.env.Mongo_URI);
-    console.log("MongoDb connected successfully");
-  } catch (error) {
-    console.error("Connection to MongoDB failed:", error);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDb;
