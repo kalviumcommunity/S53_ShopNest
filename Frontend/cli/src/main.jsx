077d4e59d277d64a2e0f0a4fe9e50c4a75f27fa2
qnasir <qnasir575@gmail.com>
2024-09-27 12:07:58 +0530

    added google authentication

--- Frontend/cli/src/main.jsx
+++ Frontend/cli/src/main.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './components/i18n.js'
 import './index.css'
+import { dark, neobrutalism, shadesOfPurple } from '@clerk/themes';
 import { BrowserRouter } from 'react-router-dom'
+import { ClerkProvider } from '@clerk/clerk-react'
 import { TooltipProvider } from './components/ui/tooltip.jsx'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -15,9 +17,17 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <TooltipProvider>
     <BrowserRouter>
+      <ClerkProvider
+        appearance={{
+          signIn: {
+            baseTheme: [shadesOfPurple],
+          }
+        }}
+        publishableKey={PUBLISHABLE_KEY} >
         <React.Suspense fallback="loading...">
           <App />
         </React.Suspense>
+      </ClerkProvider>
     </BrowserRouter>
   </TooltipProvider>
 )
